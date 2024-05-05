a2067c975f7cf3cb51a674fb78a79e61cc1c0b44
Klaus Cedillo <klausmcedillo@gmail.com>
2024-05-04 20:23:01 -0600

    API Proteccion

--- src/services/movies/getTopRatedMovies.ts
+++ src/services/movies/getTopRatedMovies.ts
@@ -2,8 +2,7 @@ import httpInstance from "../httpInstance";
 
 export const getTopRated = async()=>{
     let res: any;
-    // const endpoint = `popular?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`
-    const endpoint = `top_rated?api_key=af6e0e60e8eaa75280ad9c6088641862&language=en-US`
+    const endpoint = `top_rated?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`
     
     await httpInstance.get(endpoint).then((data) =>{
         res = data;
